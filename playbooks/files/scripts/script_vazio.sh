92cf0124c20b6a32ce7ae1ea1be1c85b64f6122a
Marcos <marques@nic.br>
2025-02-25 16:16:20 -0300

    Teste de diretórios

--- playbooks/files/scripts/script_vazio.sh
+++ playbooks/files/scripts/script_vazio.sh
@@ -1,4 +1,5 @@
 # No direório files é onde está setado no ansible.cfg para colocar tudo que for enviado a partir do ansible
 
 ## Se eu quero enviar uma pasta é necessário a pasta estar dentro do diretório files
- 
+
+### Por segurança essa pasta não contém scripts 
